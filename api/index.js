6bebae9442a8b2b88e4c386b3e12c5365a3a5a26
realestone <lukas.jasinskas77@gmail.com>
2025-10-18 10:27:32 +0300

    feat: add hello world express server

---
+++ api/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello Leial API!" });
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`✅ Server running on http://localhost:${PORT}`);
+});
